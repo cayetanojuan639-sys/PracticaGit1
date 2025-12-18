3c0b39cd03a22ade8c6268aad4d9ffd39a8ced38
nayeli-ship-it <uzz.peron@gmail.com>
2025-12-18 08:22:09 -0600

    Presentacion de Nayeli modificado

---
+++ nayeli.py
@@ -0,0 +1,6 @@
+print('Mi nombre es: Nayeli Alexandra Peron Nava')
+print('Mi domicilio es: Huixquilucan Centro')
+print('Mi color favorito es: Morado y Rosa')
+print('Mi hobbie es: Salir con mis amigas')
+
+#programa hecho por Nayeli
